936eff4626237e3f426f8f5e1320b24cf8566694
zane-uw <35039491+zane-uw@users.noreply.github.com>
2019-03-11 11:41:45 -0700

    Add files via upload

---
+++ analysis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash 
+## analysis.sh
+
+# Download the sonnets
+
+
+# Trim introduction and concluding lines
+
+
+# Remove leading blank characters 
+
+ 
+# Split sonnets into individual files. This will involve *many* commands.
+
+
+
+# Find the longest sonnet (most words)
+
+
+# Search for specific words in  the sonnets
+
---
+++ session.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash 
+## session.sh
+
+## Use this file to record the command-line commands you use in the assignment.
+## Add each command BELOW the descriptive comment:
+
+
+# Display the current working directory
+
+
+# Display the contents of the `.gitignore` file
+
+
+# Make a new `sonnets` folder inside your repo
+
+
+# Change directory into the `sonnets` folder and display its contents 
+# (2 commands)
+
+
+# Copy the `.gitignore` file into the `sonnets` folder without changing 
+# directories
+
+
+# Display the contents of the `sonnets` folder to show it contains the hidden
+# `.gitignore` filer
+
+
+# Add the `sonnets` folder to git
+
+
+# Commit your change to git (with a descriptive message)
+
